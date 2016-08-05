c5c018c7d6bee621295f957b72daee509f257eed
uno.kim <uno.kim@yellomobile.com>
2016-08-05 19:46:18 +0900

    누락된 소스 커밋

---
+++ sample/src/main/java/kim/uno/kotlin/sample/Sample.java
@@ -0,0 +1,15 @@
+package kim.uno.kotlin.sample;
+
+import kim.uno.kotlin.base.item.RecyclerItem;
+
+/**
+ * Created by Uno.kim on 2016. 8. 5..
+ */
+public class Sample implements RecyclerItem {
+
+    @Override
+    public int getViewType() {
+        return 0;
+    }
+
+}
